f7f0af8685c3ce0f41a4945abe130df7a1e90964
MatheusGh0st <matheusghost88@gmail.com>
2024-07-24 18:21:02 -0300

    finish navbar with login and register

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -3,7 +3,7 @@
     <div class="md:hidden lg:block w-[20%]">
       <router-link to="/" class="font-bold text-xl text-blue-600">NYXSOCIAL</router-link>
     </div>
-    <div class="hidden md:flex w-[-50%] text-sm">
+    <div class="hidden md:flex w-[-50%] text-sm items-center justify-between">
     </div>
     <div class="flex gap-6 text-gray-600">
       <router-link to="/" class="flex items-center gap-2">
@@ -11,15 +11,23 @@
         <span>Homepage</span>
       </router-link>
       <router-link to="/" class="flex items-center gap-2">
-        <img src="../../public/friends.png" alt="Homepage" width="16px" height="16px" class="w-4 h-4"><img />
+        <img src="../../public/friends.png" alt="Friends" width="16px" height="16px" class="w-4 h-4"><img />
         <span>Friends</span>
       </router-link>
       <router-link to="/" class="flex items-center gap-2">
-        <img src="../../public/stories.png" alt="Homepage" width="16px" height="16px" class="w-4 h-4" ><img />
+        <img src="../../public/stories.png" alt="Stories" width="16px" height="16px" class="w-4 h-4" ><img />
         <span>Stories</span>
       </router-link>
     </div>
-    <div class="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+    <div class="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+      <input type="text" placeholder="search..." class="bg-transparent outline-none"/>
+      <img src="../../public/search.png" alt="" width="14" height="14"><img/>
+    </div>
+    <div class="w-[20%] flex items-center gap-4 xl:gap-8 justify-end">
+      <div class="flex items-center gap-2 text-sm">
+        <img src="../../public/login.png" alt="Login/Register" width="20px" height="20px"><img />
+        <router-link to="/sign-in">Login/Register</router-link>
+      </div>
       <MobileMenu />
     </div>
   </div>
