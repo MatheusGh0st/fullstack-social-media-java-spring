7582c88d313c4779e8930fec6bccca841f7163e1
MatheusGh0st <matheusghost88@gmail.com>
2024-07-26 13:47:31 -0300

    add new components Feed and Post tailwind css styles and html

--- client/src/components/Feed.vue
+++ client/src/components/Feed.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-   Feed
+  <div class="p-4 bg-white shadow-md rounded-lg flex flex-col gap-12">
+    <Post />
+    <Post />
   </div>
 </template>
 
 <script setup>
+import Post from "./Post.vue";
   </script>
